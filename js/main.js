82a5a0d110ee3027e2586682752f5c0ecc114ffb
Mariia Mytsko <mytsko.mariia@gmail.com>
2022-02-20 00:13:20 +0100

    adding functions render, anyadir letra

--- js/main.js
+++ js/main.js
@@ -6,7 +6,9 @@ const solucoin = listadoPosiblesSoluciones[Math.floor(Math.random()*listadoPosib
 const visualJugador = document.querySelector('#visual-jugador');
 const nuevaLetra = document.querySelector('#nueva-letra');
 const letrasJugador = document.querySelector('#letras-jugador');
-let visualUsuario = Array(solucoin.length).fill('_');
+const mensajeGanado = document.querySelector('#mensaje-ganado');
+const mensajePerdido = document.querySelector('#mensaje-perdido');
+const visualUsuario = Array(solucoin.length).fill('_');
 let intentos = 6;
 let letrasIntroducidas = [];
 
@@ -18,9 +20,54 @@ let letrasIntroducidas = [];
  */
 
 /**
+ * renderiza el estado de la palabra a adivinar
+ * @param {string} solucionLocal palabra a adivinar
+ * @param {Array} letrasUsuario letras introducidas por el usuario
+ * @param {HTMLElement} targetAdivinadas lugar donde se mostrara el estado de la adivinacion
+ * @param {string}vacio caracter de letra aun no adivinada
+ * @param {string}separador separador entre los caracteres mostrados
+ */
+function render(solucionLocal,letrasUsuario,targetAdivinadas,vacio='_',separador=" "){
+    //letra y guiones
+    const finalHTML = solucionLocal.split('').map(function (letra){
+        return letrasUsuario.includes(letra)
+            ? letra
+            : vacio;
+    });
+    //imprimimos
+    targetAdivinadas.textContent= finalHTML.join(separador);
+}
+
+/**
+ * anyade la letra del usuario
+ * @param event
+ */
+function anyadirLetra(){
+    //ercogemos la letra
+    const miNuevaLetra = nuevaLetra.value.toLowerCase();
+    //la guardamos
+    letrasIntroducidas.push(miNuevaLetra);
+    //limpiamos la letra introducida
+    nuevaLetra.value = '';
+    //renderizamos
+    render(solucoin,letrasIntroducidas,visualJugador);
+
+}
+
+
+/**
  * eventos
  */
+
+nuevaLetra.addEventListener('keyup',function (event){
+    if(event.key==="Enter"){
+        anyadirLetra();
+        render(solucoin,letrasIntroducidas,visualJugador);
+    }
+})
 
 /**
  * inicio
  */
+
+render(solucoin,letrasIntroducidas,visualJugador);
